d2fe8857bed25e006c7a432762f185ac4858baed
houlder.hambalaye <hnahoulder@gmail.com>
2018-06-26 18:23:36 +0200

    init

---
+++ src/app/heroes/hero-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    template: `
+    <h2>HEROES</h2>
+
+  `
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
